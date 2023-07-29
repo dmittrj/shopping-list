3ec8d7b643b4d1f9c3efd497f67012852ada6552
Дмитрий Балабанов <dmittrj@outlook.com>
2023-07-29 18:30:22 +0300

    Editing items

--- assets/js/control.js
+++ assets/js/control.js
@@ -62,7 +62,7 @@ function ui_create_item(name, cost, amount) {
 }
 
 
-function ui_create_input() {
+function ui_create_input(action_by_enter) {
   const ele_ListItemTextInput = document.createElement('input');
   ele_ListItemTextInput.classList.add('shoplist-list-item-textbox');
   ele_ListItemTextInput.setAttribute('spellcheck', 'false');
@@ -71,9 +71,15 @@ function ui_create_input() {
       if (ele_ListItemTextInput.value === '') {
         ui_turn_input_to_add(ele_ListItemTextInput.parentElement.parentElement);
       } else {
-        let new_item_content = parse_item(ele_ListItemTextInput.value);
-        sl_append_item(new_item_content);
-        ui_append_item(new_item_content);
+        if (action_by_enter == 'add') {
+          let new_item_content = parse_item(ele_ListItemTextInput.value);
+          sl_append_item(new_item_content);
+          ui_append_item(new_item_content);
+        } else if (action_by_enter == 'edit') {
+          let edited_item = ui_turn_input_to_item(ele_ListItemTextInput.parentElement.parentElement);
+          let edited_item_text = edited_item.querySelector('.shoplist-list-item-text')
+          edited_item_text.addEventListener('click', () => { display_edit_item_field(edited_item) });
+        }
       }
     }
   });
@@ -84,6 +90,7 @@ function ui_create_input() {
 
 function ui_append_item(item) {
   var ele_ListItem = ui_create_item(item.name, item.cost, item.amount);
+  ele_ListItem.querySelector('.shoplist-list-item-text').addEventListener('click', () => {display_edit_item_field(ele_ListItem)});
 
   document.querySelector(".shoplist-list").appendChild(ele_ListItem);
   
@@ -117,10 +124,21 @@ function ui_turn_add_to_input() {
   const ele_ListItemAdd = document.querySelector('#shoplist-add-pseudoitem');
   const ele_ListItemAddText = ele_ListItemAdd.querySelector('.shoplist-list-item-text');
   
-  const ele_ListItemTextInput = ui_create_input();
+  const ele_ListItemTextInput = ui_create_input('add');
   ele_ListItemAdd.style.opacity = '1';
 
   ele_ListItemAddText.replaceWith(ele_ListItemTextInput);
+  ele_ListItemTextInput.focus();
+}
+
+
+function ui_turn_item_to_input(item) {
+  const ele_ListItemText = item.querySelector('.shoplist-list-item-text');
+  
+  const ele_ListItemTextInput = ui_create_input('edit');
+  ele_ListItemTextInput.value = ele_ListItemText.innerText;
+
+  ele_ListItemText.replaceWith(ele_ListItemTextInput);
   ele_ListItemTextInput.focus();
 }
 
@@ -137,6 +155,8 @@ function ui_turn_input_to_item(input_item) {
   let ele_ListItem = ui_create_item(parsed.name, parsed.cost, parsed.amount);
 
   input_item.replaceWith(ele_ListItem);
+
+  return ele_ListItem;
 }
 
 
@@ -172,7 +192,11 @@ function display_new_item_field() {
     ui_append_add();
     ui_turn_add_to_input();
   }
-  
+}
+
+
+function display_edit_item_field(item) {
+  ui_turn_item_to_input(item);
 }
 
 
